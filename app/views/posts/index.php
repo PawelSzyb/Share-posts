f9eec781bab25991d5eb83bdd2d0665de3007335
Paweł Szybaj <pawelszybaj@wp.pl>
2018-10-31 16:25:02 +0100

    Posts Controller

---
+++ app/views/posts/index.php
@@ -0,0 +1,12 @@
+<?php require APPROOT . "/views/inc/header.php"; ?>
+  <div class="row mt-3">
+    <div class="col-md-6">
+      <h1>Posts</h1>
+    </div>
+    <div class="col-md-6 justify-content-end d-flex ">
+      <a href="<?php echo URLROOT; ?>/posts/add" class="btn btn-primary align-self-center">
+        <i class="fas fa-pencil-alt"></i> Add Post
+      </a>
+    </div>
+  </div>
+<?php require APPROOT . "/views/inc/footer.php"; ?>
